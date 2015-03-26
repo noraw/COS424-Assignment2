e9e14580d11f157864785b6ce5ba9f6f124eff68
Nora Coler <noracoler@gmail.com>
2015-03-25 21:10:49 -0400

    fixed prediction script and generated results

--- scripts/predictionMethods.py
+++ scripts/predictionMethods.py
@@ -17,54 +17,74 @@ from math import sqrt
 
 # read a dat file back into python. 
 def read_dat_file(myfile, numofchromos):
-    array = np.fromfile(myfile, dtype=np.uint8, count=-1, sep="")
+    array = np.fromfile(myfile, dtype=np.float64, count=-1, sep="")
     array = np.reshape(array,(numofchromos,-1))
     return array
 
 
 
+def predict(clf, X, y, X_test, y_test):
+    start = timeit.default_timer()
+    clf.fit(X, y)
+    print "   predictions done.";
+    predicted = clf.predict(X_test)
+    #probs_train = clf.predict_proba(X)    
+    #probs_test = clf.predict_proba(X_test0)
+    print "   R2:      " + str(metrics.r2_score(y_test, predicted)) # classifier accuracy
+    print "   RSME:    " + str(sqrt(metrics.mean_squared_error(y_test, predicted))) # classifier accuracy
+    #print(metrics.classification_report(y_test, predicted))
+    stop = timeit.default_timer()
+    print "   runtime: " + str(stop - start)
+
+    if args.LinearRegression:
+        return clf.coef_[0]
+    if args.Lasso:
+        return clf.coef_
+    if args.RANSAC:
+        return clf.estimator_.coef_[0]
+
+
+
+
+
 # argument parsing.
 parser = argparse.ArgumentParser(description='Predict Genome.')
-parser.add_argument("-M", "--MeanValue", action="store_true", help="run mean value prediction")
 parser.add_argument("-R", "--LinearRegression", action="store_true", help="run linear regression")
 parser.add_argument("-L", "--Lasso", action="store_true", help="run Lasso")
-parser.add_argument("-G", "--Guassian", action="store_true", help="run Gaussian process")
+parser.add_argument("-S", "--RANSAC", action="store_true", help="run RANSAC process")
 
 # OTHER INPUT VARIABLES
-outname = ""
-inX0 = ""
-inX1 = ""
-inXU = ""
-inX_test0 = ""
-inX_test1 = ""
-inX_testU = ""
-inY = ""
-inY_test = ""
-numofchromos = 400
+outname = "" # assigned later
+inX0 = "../data/X0_chr1_cutoff_20_len_7523.dat"
+inXA = "../data/XA_chr1_cutoff_20_len_7523.dat"
+inXR = "../data/XR_chr1_cutoff_20_len_7523.dat"
+inX_test0 = "../data/X_test0_chr1_cutoff_20_len_368411.dat"
+inX_testA = "../data/X_testA_chr1_cutoff_20_len_368411.dat"
+inX_testR = "../data/X_testR_chr1_cutoff_20_len_368411.dat"
+inY = "../data/y_chr1_cutoff_20_len_7523.dat"
+inY_test = "../data/y_test_chr1_cutoff_20_len_368411.dat"
+numofchromos = 7523
+numofchromosTest = 368411
 
 args = parser.parse_args()
 print args;
 
 X0 = read_dat_file(inX0, numofchromos) # matrix of training feature data (1s)
-X1 = read_dat_file(inX1, numofchromos) # matrix of training feature data (1s)
-XU = read_dat_file(inXU, numofchromos) # matrix of training feature data (1s)
-X_test0 = read_dat_file(inX_test0, numofchromos) # matrix of testing feature data (0s)
-X_test1 = read_dat_file(inX_test1, numofchromos) # matrix of testing feature data (0s)
-X_testU = read_dat_file(inX_testU, numofchromos) # matrix of testing feature data (0s)
+XA = read_dat_file(inXA, numofchromos) # matrix of training feature data (1s)
+XR = read_dat_file(inXR, numofchromos) # matrix of training feature data (1s)
+X_test0 = read_dat_file(inX_test0, numofchromosTest) # matrix of testing feature data (0s)
+X_testA = read_dat_file(inX_testA, numofchromosTest) # matrix of testing feature data (0s)
+X_testR = read_dat_file(inX_testR, numofchromosTest) # matrix of testing feature data (0s)
 y = read_dat_file(inY, numofchromos) # vector of training values (1s)
-y_test = read_dat_file(inY_test, numofchromos) # vector of testing ground truth values (0s)
+y_test = read_dat_file(inY_test, numofchromosTest) # vector of testing ground truth values (0s)
 
-print "X shape" + str(X.shape)
-print "X test shape " + str(X_test.shape)
+print "X shape" + str(X0.shape)
+print "X test shape " + str(X_test0.shape)
 print "Y shape" + str(y.shape)
 print "Y test shape " + str(y_test.shape)
 
 
 # CLASSIFY!
-if args.MeanValue:
-    print "Mean Value"
-    outname = "meanValue"
-
 if args.LinearRegression:
     print "Linear Regression"
     outname = "linearRegression"
@@ -76,48 +96,34 @@ if args.Lasso:
     outname = "lasso"
     clf = linear_model.Lasso(alpha=.2)
 
-if args.Gaussian:
-    print "Gaussian"
-    outname = "guassian"
-    clf = gaussian_process.GaussianProcess(theta0=1e-2, thetaL=1e-4, thetaU=1e-1)
+if args.RANSAC:
+    print "RANSAC"
+    outname = "ransac"
+    clf = linear_model.RANSACRegressor(linear_model.LinearRegression())
 
-if args.MeanValue or args.LinearRegression or args.Lasso or args.Guassian:
+
+if args.LinearRegression or args.Lasso or args.RANSAC:
     print "Nan = 0"
     feature_importance0 = predict(clf, X0, y, X_test0, y_test)
 
-    print "Nan = 1"
-    feature_importance1 = predict(clf, X1, y, X_test1, y_test)
+    print "Nan = Average"
+    feature_importanceA = predict(clf, XA, y, X_testA, y_test)
 
-    print "Nan = Uniform"
-    feature_importanceU = predict(clf, XU, y, X_testU, y_test)
+    print "Nan = Random"
+    feature_importanceR = predict(clf, XR, y, X_testR, y_test)
 
     # output feature importance for graphs
     outfile = file("feature_importance_" + outname + ".csv", "w")
-    outfile.write('"Feature ID","0 Nans","1 Nans","Uniform Nans"\n');
+    outfile.write('"Feature ID","0 Nans","Average Nans","Random Nans"\n');
+    print len(feature_importance0)
     for i in range (len(feature_importance0)):
         outLine = str(i) + ","
         outLine += str(feature_importance0[i]) + ","
-        outLine += str(feature_importance1[i]) + ","
-        outLine += str(feature_importanceU[i])
+        outLine += str(feature_importanceA[i]) + ","
+        outLine += str(feature_importanceR[i])
         outLine += "\n"
         outfile.write(outLine)
     outfile.close();
-
-
-
-def predict(clf, X, y, X_test, y_test):
-    start = timeit.default_timer()
-    clf.fit(X0, y)
-    print "   predictions done.";
-    predicted = clf.predict(X_test0)
-    #probs_train = clf.predict_proba(X)    
-    probs_test = clf.predict_proba(X_test0)
-    print "   R2:      " + str(metrics.r2_score(y_test, predicted)) # classifier accuracy
-    print "   RSME:    " + str(sqrt(metrics.mean_squared_error(y_test, predicted))) # classifier accuracy
-    print(metrics.classification_report(y_test, predicted))
-    stop = timeit.default_timer()
-    print "   runtime: " + str(stop - start)
-    return clf.coef_
 
 
 
